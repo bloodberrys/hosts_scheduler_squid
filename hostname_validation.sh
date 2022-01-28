2dbdd1e98a2023fc387e6cb8983daba21cdbb7c7
Alfian Firmansyah <alfianvansykes@gmail.com>
2022-01-28 18:48:03 +0700

    filter and loop the multiple ip

--- hostname_validation.sh
+++ hostname_validation.sh
@@ -2,6 +2,7 @@
 
 echo -e "Script to test the hostname ip reliability."
 
+mkdir results
 IFS=" " read -r -a ips_store <<< ""
 
 endpoint="api.bni-ecollection.com api.bni.co.id dev.bni-ecollection.com apidev.bni.co.id aping-ideal.dbs.com aping.dbs.id apingid.wlb.dbs.id api.btpn.com api-mt.thunes.com api.danamon.co.id api.mailgun.net"
@@ -28,27 +29,38 @@ do
     loopcounter=0
     while [ $loopcounter -lt 10 ]
     do
-        value_store=$(dig +short "${endpoint[$counter]}")
-        lists=""
-        if [[ ! " ${ips_store[*]} " =~ " ${value_store} " ]]; then
-            ips_store+=("${value_store}")
-            length=${#ips_store[@]}
-            for ((l = 0; l < length; l++))
-            do
-                lists+="${ips_store[$l]}\n"
-            done
+        # Filter and map hostname's ips
+        mapfile -t value_store < <(dig +short "${endpoint[$counter]}" | grep -oE '\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b' | tr '\n' ' ')
+        value_store_length=${#value_store[@]}
+        for ((ipcounter = 0; ipcounter < value_store_length; ipcounter++))
+        do
+            lists=""
+            if [[ ! " ${ips_store[*]} " =~ " ${value_store[$ipcounter]} " ]]; then
+                length=${#ips_store[@]}
+                for ((l = 0; l < length; l++))
+                do
+                    ips_store+=("${value_store[$ipcounter]}")
+                    lists+="${ips_store[$l]}\n"
+                done
 
-            if [[ "$loopcounter" != 0 ]]; then
-            echo -e "IP CHANGED to $value_store\nIP LISTS:\n$lists"
-            echo -e "IP CHANGED to $value_store\nIP LISTS:\n$lists" >> $logname
+                if [[ "$loopcounter" != 0 ]]; then
+                echo -e "IP CHANGED to ${value_store[$ipcounter]}\nIP LISTS:\n$lists"
+                echo -e "IP CHANGED to ${value_store[$ipcounter]}\nIP LISTS:\n$lists" >> $logname
+                fi
             fi
-        fi
+        done
+
         for ((i = 0; i < ${#ips_store[@]}; i++))
         do
             ip="${ips_store[$i]}"
-            status_code=$(curl -sI https://$ip --insecure -m 5 | grep -Po "[0-9]{2,3}+" | head -n 1)
+            status_code=$(curl -sI https://$ip --insecure -m 2 | grep -Po "[0-9]{2,3}+" | head -n 1)
+            if [ -n "$status_code" ]; then
+            echo "[$(date)] ${ips_store[$i]} timeout"
+            echo "[$(date)] ${ips_store[$i]} timeout" >> $logname
+            else
             echo "[$(date)] ${ips_store[$i]} $status_code"
             echo "[$(date)] ${ips_store[$i]} $status_code" >> $logname
+            fi
         done
         ((loopcounter=loopcounter+1))
         sleep 0.5
