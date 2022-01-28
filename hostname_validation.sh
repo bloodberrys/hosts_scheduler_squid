c0ea23763565ae8f967c287dbff30624d543b508
Alfian Firmansyah <alfianvansykes@gmail.com>
2022-01-28 17:42:47 +0700

    baked in array

--- hostname_validation.sh
+++ hostname_validation.sh
@@ -1,52 +1,53 @@
 #!/bin/bash
 
-echo -e "This script is created to test whether the hostname and IP is seamless or not"
+echo -e "Script to test the hostname ip reliability."
 
 IFS=" " read -r -a ips_store <<< ""
 
-endpoint="api.bni-ecollection.com"
-echo -e "HOSTNAME: $endpoint"
-counter=1
-loopcounter=0
-logname="result.log"
-if [ -f "$logname" ]; then
-    namerotator=$(ls $pwd | grep -Po "${logname}[\.0-9]*" | tail -n 1 | awk "{sub(/${logname}[\.]*/,\"\")}1")
-    namerotator=$((namerotator+1))
-    logname="$logname.$namerotator"
-    echo $logname
-else
-    logname="result.log"
-fi
-while true
+endpoint="api.bni-ecollection.com api.bni.co.id"
+IFS=" " read -r -a endpoint <<< "$endpoint"
+endpointlength=${#endpoint[@]}
+for ((counter = 0; counter < endpointlength; counter++))
 do
-    value_store=$(dig +short $endpoint)
-    lists=""
-    if [[ ! " ${ips_store[*]} " =~ " ${value_store} " ]]; then
-        ips_store+=("${value_store}")
-        length=${#ips_store[@]}
-        for ((l = 0; l < length; l++))
-        do
-            lists+="${ips_store[$l]}\n"
-        done
+    echo -e "HOSTNAME: ${endpoint[@counter]}"
 
-        if [[ "$loopcounter" != 0 ]]; then
-        echo -e "IP CHANGED to $value_store\nIP LISTS:\n$lists"
-        echo -e "IP CHANGED to $value_store\nIP LISTS:\n$lists" >> $logname
-        fi
-    fi
-    for ((i = 0; i < ${#ips_store[@]}; i++))
-    do
-        ip="${ips_store[$i]}"
-        status_code=$(curl -sI https://$ip --insecure | grep -Po "[0-9]{2,3}+" | head -n 1)
-        echo "[$(date)] ${ips_store[$i]} $status_code"
-        echo "[$(date)] ${ips_store[$i]} $status_code" >> $logname
-    done
-
-    if [ "$counter" -eq 9 ];then
-    counter=1
+    # Log rotate for each endpoint
+    logname="result.log"
+    if [ -f "$logname" ]; then
+        namerotator=$(ls $pwd | grep -Po "${logname}[\.0-9]*" | tail -n 1 | awk "{sub(/${logname}[\.]*/,\"\")}1")
+        namerotator=$((namerotator+1))
+        logname="$logname.$namerotator"
+        echo $logname
     else
-    ((counter=counter+1))
+        logname="result.log"
     fi
-    ((loopcounter=loopcounter+1))
-    sleep 0.5
+
+    loopcounter=0
+    while [ $loopcounter -lt 10 ]
+    do
+        value_store=$(dig +short ${endpoint[@counter]})
+        lists=""
+        if [[ ! " ${ips_store[*]} " =~ " ${value_store} " ]]; then
+            ips_store+=("${value_store}")
+            length=${#ips_store[@]}
+            for ((l = 0; l < length; l++))
+            do
+                lists+="${ips_store[$l]}\n"
+            done
+
+            if [[ "$loopcounter" != 0 ]]; then
+            echo -e "IP CHANGED to $value_store\nIP LISTS:\n$lists"
+            echo -e "IP CHANGED to $value_store\nIP LISTS:\n$lists" >> $logname
+            fi
+        fi
+        for ((i = 0; i < ${#ips_store[@]}; i++))
+        do
+            ip="${ips_store[$i]}"
+            status_code=$(curl -sI https://$ip --insecure | grep -Po "[0-9]{2,3}+" | head -n 1)
+            echo "[$(date)] ${ips_store[$i]} $status_code"
+            echo "[$(date)] ${ips_store[$i]} $status_code" >> $logname
+        done
+        ((loopcounter=loopcounter+1))
+        sleep 0.5
+    done
 done
